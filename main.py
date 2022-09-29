6929ae5140e8816981d1e6b151ee41e28c22894c
Kat-1324 <katya.milyaeva@gmail.com>
2022-09-29 13:42:29 +0100

    Delete main.py
    Newer version of the script is available.

--- main.py
+++
@@ -1,7 +0,0 @@
-from coinbase_pro_arbitrage_detection import coinbase_main
-
-# currencies = ['USD', 'BTC', 'ETH']
-# transaction_fee = 0.02
-# balance = {'USD': 1000, 'BTC': 0.98, 'ETH': 500}
-
-# coinbase_main(currencies, transaction_fee, balance)
